3b610e0007f41d2341d0a71e5f8c6f6b040cfc7c
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-09-27 13:17:20 -0500

    creat instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,30 @@ class Puppy
     toy
   end
 
+  def speak(int)
+  int.times	{puts "Woof!"} 
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(int)
+  	age = int * 7
+  end
+
+  def hungry
+   true
+  end
 end
+
+puppy = Puppy.new
+
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+p puppy.dog_years(3)
+puts "Is the puppy hungry? #{puppy.hungry}"
+
+
 
