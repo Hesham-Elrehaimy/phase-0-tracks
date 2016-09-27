1ec26cd4a4e5967a11c32a40ce6b7070f75dd029
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-09-27 13:46:02 -0500

    create a new class with instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -20,6 +20,12 @@ class Puppy
   def hungry
    true
   end
+
+  def initialize
+
+  	puts "Initializing new puppy instance ..."
+  	
+  end
 end
 
 puppy = Puppy.new
@@ -29,6 +35,61 @@ puppy.speak(3)
 puppy.roll_over
 p puppy.dog_years(3)
 puts "Is the puppy hungry? #{puppy.hungry}"
+
+
+class Cats
+
+  def initialize
+  	puts "Initializing new cat instance ..."
+  end
+
+  def speak(int)
+
+  	int.times	{puts "Meow!"} 
+  	
+  end
+
+  def name(str)
+
+  	str
+  end	
+
+end
+
+# cat = Cats.new
+# puts "My name is #{cat.name("Fluffy")}"
+# cat.speak(3)
+
+
+instances = []
+
+i = 0
+
+while i < 50
+
+instances << Cats.new
+
+i += 1
+
+end
+
+
+i = 1
+instances.each do |cat|
+
+	cat.speak(2)
+	puts "#{cat.name("Fluffy")} #{i}"
+i += 1	
+end
+
+
+
+
+
+
+
+
+
 
 
 
