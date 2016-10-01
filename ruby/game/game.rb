9d5d260d3a9b732dd07297056592ad66900b595f
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-01 12:19:20 -0500

    add method to insert guess to dashed word

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -5,20 +5,21 @@
 # name: The Guessing Game
 # player1 input: a word
 # player2 input: a letter
-# number of guesses: the number of unrepeated letters in player1's word
+# number of guesses: the number of letters in player1's word (repeated guess does not count)
 # output: an updated version of an empty string containing the letters in player1's word which player2's letter matched
 # final output: a congratulatory message if player2 wins or a taunting message if player2 loses  
 
 
 # BEHAVIOR:
 # stores input
-# compares both players' input
-# keeps track of number of guesses
 # turns player1's input into dashes
-# prints messages indicating player2's progress
+# keeps track of number of guesses
+# checks if player2's guess matches a letter in player1's word
+# replaces a dash with player2's correct guess
 # prints a message of whether player2 won or lost
 
 class Game
+          # attr_reader :word, :dashed_word
 
 	def initialize(word)
 
@@ -26,13 +27,19 @@ class Game
 	end
 
 	def dasher
-		dashed_word = '-' * @word.length
-		dashed_word
+		@dashed_word = '-' * @word.length
+		@dashed_word
 	end	
 
-
+    def compare(letter)
+    	if @word.include? letter
+    		@dashed_word[@word.index(letter)] = @word[@word.index(letter)]
+    		@dashed_word
+    	else
+    		@dashed_word
+    		
+    	end
+    end	
 
 end		
 
-game = Game.new('hesham')
-p game.dasher
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,10 +1,14 @@
 require_relative 'game'
 
 describe Game do 
-	let(:game) { Game.new('word')}
+	let(:game) { Game.new('hesham')}
     it "turns player1's input into dashes"	do
-      expect(game.dasher).to eq "----"	
+      expect(game.dasher).to eq "------"	
     end
+    
+    it "inserts the correctly guessed letter into the dashes" do
+    	expect(game.compare('h')).to eq "h-----"
+    end	
 
 end
 
