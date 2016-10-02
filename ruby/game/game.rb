87c20fb52f9b47dae525a6f1f78668fbb57c01fb
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-02 05:39:56 -0500

    add methods to the game class

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -16,30 +16,76 @@
 # keeps track of number of guesses
 # checks if player2's guess matches a letter in player1's word
 # replaces a dash with player2's correct guess
+# a repeated guess does not count
 # prints a message of whether player2 won or lost
 
 class Game
-          # attr_reader :word, :dashed_word
+          attr_accessor :dashed_word, :word, :guess_count
 
-	def initialize(word)
+    def initialize(word)
 
-		@word = word
-	end
+        @word = word
+        @guess_count = 0
+    end
 
-	def dasher
-		@dashed_word = '-' * @word.length
-		@dashed_word
-	end	
+    def dasher
+        @dashed_word = '-' * @word.length
+        @dashed_word
+    end 
 
     def compare(letter)
-    	if @word.include? letter
-    		@dashed_word[@word.index(letter)] = @word[@word.index(letter)]
-    		@dashed_word
-    	else
-    		@dashed_word
-    		
-    	end
-    end	
+        if @word.include? letter
+            @guess_count += 1 
+            @dashed_word[@word.index(letter)] = @word[@word.index(letter)]
+            @dashed_word
+        else
+            @guess_count += 1 
+            @dashed_word
+        end
+      
+    end 
 
-end		
+
+    
+
+end 
+
+puts "Player1: Enter a word for player2 to guess:"
+word = gets.chomp
+game = Game.new(word)
+puts game.dasher
+while game.guess_count < game.word.length
+    puts "player2: Enter a letter to guess player1's word (You have #{(game.word.length).to_i - (game.guess_count).to_i} guesses):"
+    letters = []
+    
+
+    letter = gets.chomp
+    letters << letter
+    guesses = Hash.new(0)
+letters.each{ |e| guesses[e] += 1 }  
+   
+   if game.compare(letter)
+    puts game.dashed_word
+   elsif
+    guesses[:e] > 1
+    game.guess_count += 0
+   else
+    puts game.dashed_word
+   end
+end   
+
+if game.dashed_word == game.word
+
+    puts "Congratulations. You've guessed the word correctly."
+else
+    puts "*In Eric Cartman's voice* Nana nananana! Haha hahahaha! You couldn't guess the word."
+end        
+
+ # game = Game.new('hesham')
+ 
+ # puts game.dasher
+ 
+ # puts game.compare('h')
+
+ # puts game.guess_count
 
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,14 +1,21 @@
 require_relative 'game'
 
 describe Game do 
-	let(:game) { Game.new('hesham')}
+	let(:game) { Game.new('hesham') }
+
     it "turns player1's input into dashes"	do
       expect(game.dasher).to eq "------"	
     end
     
-    it "inserts the correctly guessed letter into the dashes" do
-    	expect(game.compare('h')).to eq "h-----"
-    end	
+    it "replaces a dash with the correctly guessed letter"	do
+      game.dasher
+      expect(game.compare('h')).to eq "h-----"	
+    end
+
+    it "returns the same dashed string when player2 guesses wrong" do
+      game.dasher
+      expect(game.compare('i')).to eq "------"
+    end  
 
 end
 
