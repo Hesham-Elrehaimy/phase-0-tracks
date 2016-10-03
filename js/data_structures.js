96f24bcc6a6996f35af2a6a892c82a07e98a4805
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-03 18:28:08 -0500

    write a constructor function for a car

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,19 @@ for (var i=0; i < names.length; i++){
   horse[names[i]]=colors[i];
 }
 console.log(horse);
+
+function Car(model, year, automatic) {
+
+	this.model = model;
+	this.year = year;
+	this.automatic = automatic;
+
+	this.stop = function() {console.log('stops')};
+	console.log('car initialization complete');
+}
+
+var newCar = new Car('honda', 2016, true);
+
+newCar.stop();
+
+console.log(newCar);
