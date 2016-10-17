9e7aeceb44b7e7cd455ef27d3448d52cdcff2111
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-16 20:30:06 -0500

    create a fibonacci method

--- ruby/problem_solving_methods_jackrabbits.rb
+++ ruby/problem_solving_methods_jackrabbits.rb
@@ -15,4 +15,26 @@ arr = [42, 89, 23, 1]
 p search_array(arr, 1)
 p search_array(arr, 24)
 
+def fib(num)
+	
+	result = [0, 1]
+	
+ i = 2 
+ 
+ while i < num
+ 
+ result << result[i-1] + result[i-2]
+ 
+ i += 1
+ 
+ end
+ 
+ result
+ 
+	
+end
+
+p fib(100)
+p fib(100)[-1]
+
 	
