fa339af02263e8d829e0b3bdcd12ab0080771234
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-16 21:19:20 -0500

    create a sorting method

--- ruby/problem_solving_methods_jackrabbits.rb
+++ ruby/problem_solving_methods_jackrabbits.rb
@@ -37,4 +37,26 @@ end
 p fib(100)
 p fib(100)[-1]
 
+def bubble_sort(array)
+  n = array.length
+ 
+    swapped = true
+    while swapped
+	swapped = false
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+  end
+
+  array
+end
+
+arr = [0, 5 , 10 , 2 , 1100]
+
+p bubble_sort(arr)
+
 	
