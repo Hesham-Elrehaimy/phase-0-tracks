e55ea4eb411b4b56c279c2cd037901fa198fbc7f
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-18 22:20:29 -0500

    create a puppy class

---
+++ ruby/puppy_jackrabbits.rb
@@ -0,0 +1,14 @@
+# BREED -----------
+# Labrador
+
+# CHARACTERISTICS -----
+# Name: Felfel
+# Color: Black
+# Friendly: Yes
+# Age: 2
+# Size: average
+
+# BEHAVIOR --------
+#Catch_treat
+#Shake_hands
+#Bark
