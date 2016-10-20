a56f5882246803eea509faab72dc3cede220b05e
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-19 22:38:22 -0500

    create multiple santas

---
+++ ruby/santa_jackrabbits.rb
@@ -0,0 +1,54 @@
+class Santa
+
+	attr_writer :gender
+	attr_reader :ethnicity, :reindeer_ranking
+	attr_accessor :age
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+		puts "Initializing Santa instance ..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!" 
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!" 
+	end
+
+	def celebrate_birthday
+		@age += 1
+	end
+	
+	def get_mad_at(reindeer)
+		@reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer)))
+		p @reindeer_ranking
+	end
+
+end
+
+# clause = Santa.new("female", "white")
+# clause.speak
+# clause.eat_milk_and_cookies('Oreo')
+# clause.celebrate_birthday
+# p clause.age
+# clause.get_mad_at('Vixen')
+# clause.gender = "male"
+# p clause.ethnicity
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+santas = []
+i = 1
+# 5.times do 
+	santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+	santas.each do |santa|
+		santa.age = rand(140)
+		puts "Santa number #{i} is a #{example_ethnicities.sample} #{example_genders.sample}. This santa is #{santa.age} years old and has reindeers named #{santa.reindeer_ranking} ordered from most to least favorite."
+  i += 1
+	end
+# end
+
