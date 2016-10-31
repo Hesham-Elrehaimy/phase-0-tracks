ac6e5293d23f4bb4574e413ac38f65e8bb7fd4af
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-10-30 21:41:04 -0500

    create a car class

--- js/data_structures_jackrabbits.js
+++ js/data_structures_jackrabbits.js
@@ -9,4 +9,32 @@ for (var i = 0; i < colors.length; i++) {
 horses[names[i]] = colors[i];
 }
 
-console.log (horses);
+
+function Car(brand, year, automatic) {
+	this.brand = brand;
+	this.year = year;
+	this.automatic = automatic;
+	this.honk = function() {console.log("beep beep!");};
+}
+
+toyota = new Car('Camry', 2006, true)
+console.log(toyota.brand)
+console.log(toyota.year)
+console.log(toyota.automatic)
+toyota.honk();
+
+console.log('---------');
+
+hyundai = new Car('Verna', 2009, false);
+console.log(hyundai.brand)
+console.log(hyundai.year)
+console.log(hyundai.automatic)
+hyundai.honk();
+
+console.log('---------');
+
+nissan = new Car('Sunny', 2011, true);
+console.log(nissan.brand)
+console.log(nissan.year)
+console.log(nissan.automatic)
+nissan.honk();
