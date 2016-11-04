8ae4c5c8f75db6e905a8d76b4282bfec304095d3
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-11-04 17:40:25 -0500

    create a method to add a contact

--- databases/phone_directory/phone_directory.rb
+++ databases/phone_directory/phone_directory.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
 pd = SQLite3::Database.new("directory.db")
+pd.results_as_hash = true
 
 create_table_cmd = <<-SQL 
 CREATE TABLE IF NOT EXISTS contacts(
@@ -14,3 +15,8 @@ SQL
 
 pd.execute(create_table_cmd)
 
+def add_contact(pd, name, phone_number, email, family)
+	pd.execute("INSERT INTO contacts (name, phone_number, email, family) VALUES (?, ?, ?, ?)", [name, phone_number, email, family])	
+end
+
+# add_contact(pd, "Hesham Elrehaimy", 6302769629, "hrehaimy@yahoo.com", "true")
