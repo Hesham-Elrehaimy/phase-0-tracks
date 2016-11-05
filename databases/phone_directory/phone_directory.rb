4c7c2f19e0d4c13433d9bc2a4da3631f2ce7b6cd
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-11-05 18:46:22 -0500

    fixed the boolean problem

--- databases/phone_directory/phone_directory.rb
+++ databases/phone_directory/phone_directory.rb
@@ -42,57 +42,54 @@ def view_all_contacts(db)
 	end
 end
 
-edit_contact(db, "Moamen Abdelaziz", "name", "family", "true")
-view_all_contacts(db)
+puts "To add a contact type 'add'. To edit a contact type 'edit'. To delete a contact type 'delete'. To find a contact type 'find'. To view all your contacts type 'view all':"
 
-# puts "To add a contact type 'add'. To edit a contact type 'edit'. To delete a contact type 'delete'. To find a contact type 'find'. To view all your contacts type 'view all':"
+command = gets.chomp
 
-# command = gets.chomp
-
-# if command == "add"
-# 	puts "Please type the contact's full name:"
-# 	name = gets.chomp
-# 	puts "Please type the contact's phone number:"
-# 	number = gets.chomp
-# 	puts "Please type the contact's email address:"
-# 	email = gets.chomp
-# 	puts "Is the contact a family member? Please answer with 'true' or 'false':"
-# 	family = gets.chomp
-# 	add_contact(db, name, number, email, family)
-# elsif command == "edit"
-# 	puts "Please type the full name of the contact you'd like to edit:"
-# 	full_name = gets.chomp
-# 	puts "To Edit the name type 'name'. To type the number enter 'phone_number'. To edit the email type 'email'. To change a contact's family status type 'family':" 
-# 	info = gets.chomp
-# 		if  info == "phone_number"
-# 			puts "Please type the new phone number:"
-# 			new_number = gets.chomp.to_i
-# 			edit_contact(db, full_name, "name", info, new_number)		
-# 		elsif info == "name"
-# 			puts "Please type the new name:"
-# 			new_name = gets.chomp
-# 			edit_contact(db, full_name, "name", info, new_name)	
-# 		elsif info == "email"
-# 			puts "Please type the new email:"
-# 			new_email = gets.chomp
-# 			edit_contact(db, full_name, "name", info, new_email)	
-# 		elsif info == "family"
-# 			puts "Please type the new status:"
-# 			new_status = gets.chomp
-# 			edit_contact(db, full_name, "name", info, new_status)	
-# 		else 
-# 			puts "invalid information"
-# 		end	
-# elsif command == "delete"
-# 	puts "Please type the full name of the contact you'd like to delete:"
-# 	contact_name = gets.chomp
-# 	delete_contact(db, "name", contact_name)
-# elsif command == "find"
-# 	puts "Please type the full name of the contact you'd like to find:"
-# 	name_to_find = gets.chomp
-# 	find_contact(db, "name", name_to_find)
-# elsif command == "view all"
-# 	view_all_contacts(db)
-# else
-# 	puts "invalid command"
-# end
+if command == "add"
+	puts "Please type the contact's full name:"
+	name = gets.chomp
+	puts "Please type the contact's phone number:"
+	number = gets.chomp
+	puts "Please type the contact's email address:"
+	email = gets.chomp
+	puts "Is the contact a family member? Please answer with 'true' or 'false':"
+	family = gets.chomp
+	add_contact(db, name, number, email, family)
+elsif command == "edit"
+	puts "Please type the full name of the contact you'd like to edit:"
+	full_name = gets.chomp
+	puts "To Edit the name type 'name'. To type the number enter 'phone_number'. To edit the email type 'email'. To change a contact's family status type 'family':" 
+	info = gets.chomp
+		if  info == "phone_number"
+			puts "Please type the new phone number:"
+			new_number = gets.chomp.to_i
+			edit_contact(db, full_name, "name", info, new_number)		
+		elsif info == "name"
+			puts "Please type the new name:"
+			new_name = gets.chomp
+			edit_contact(db, full_name, "name", info, new_name)	
+		elsif info == "email"
+			puts "Please type the new email:"
+			new_email = gets.chomp
+			edit_contact(db, full_name, "name", info, new_email)	
+		elsif info == "family"
+			puts "Please type the new status:"
+			new_status = gets.chomp
+			edit_contact(db, full_name, "name", info, new_status)	
+		else 
+			puts "invalid information"
+		end	
+elsif command == "delete"
+	puts "Please type the full name of the contact you'd like to delete:"
+	contact_name = gets.chomp
+	delete_contact(db, "name", contact_name)
+elsif command == "find"
+	puts "Please type the full name of the contact you'd like to find:"
+	name_to_find = gets.chomp
+	find_contact(db, "name", name_to_find)
+elsif command == "view all"
+	view_all_contacts(db)
+else
+	puts "invalid command"
+end
