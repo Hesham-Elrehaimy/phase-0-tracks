3bf1a87911e2294342245c71835742333b8a3fb1
Hesham Elrehaimy <hrehaimy@yahoo.com>
2016-11-10 22:42:35 -0600

    use jquery on a webpage

---
+++ web_dev/jquery/test.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+//     $("h1").mouseenter(function(){
+//         $("h1").hide();
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("p").mouseleave(function(){
+//         $("p").hide();
+//     });
+// });
+
+$(document).ready(function(){
+    $("#b_button").click(function(){
+        $("body").css("background-color","pink");;
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#h_button").click(function(){
+        $("h1").text("Hello world!");
+    });
+});
